c4f0df158b31202aa5f27cf43bc947550fceea4e
LAPTOP-HJ5CIGLB\hcutv <hcutvaric@gmail.com>
2026-06-11 23:29:10 +0200

    Fix integration test: disable CSRF in test mode

--- tests/integration/test_app.py
+++ tests/integration/test_app.py
@@ -11,6 +11,7 @@ from app import app, db
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
+    app.config['WTF_CSRF_ENABLED'] = False
     with app.test_client() as client:
         with app.app_context():
             db.create_all()
